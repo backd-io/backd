74982b4a32e509423d01c1de8967f8571bb74c6a
Antonio Fernandez Vara <antoniofernandezvara@gmail.com>
2019-01-06 00:44:31 +0100

    added a function to write the path of the request

--- backd/paths.go
+++ backd/paths.go
@@ -1,5 +1,48 @@
 package backd
 
+import (
+	"net/url"
+	"path"
+)
+
+type microservice int
+
+const (
+	admin microservice = iota
+	auth
+	objects
+)
+
+// auth paths
 const (
 	pathSession string = "session"
 )
+
+// admin paths
+const (
+	pathBootstrap = "bootstrap"
+)
+
+func (b *Backd) buildPath(m microservice, route string) string {
+
+	var (
+		urlString string
+	)
+
+	switch m {
+	case admin:
+		urlString = b.adminURL
+	case auth:
+		urlString = b.authURL
+	case objects:
+		urlString = b.objectsURL
+	}
+
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+	u.Path = path.Join(u.Path, route)
+	return u.String()
+
+}
