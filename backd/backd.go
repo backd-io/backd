9eb6175f38b850281211bf1ecbaf4d68b9df3345
Antonio Fernandez Vara <antoniofernandezvara@gmail.com>
2019-01-06 00:45:02 +0100

    added bootstrap function to the backd client

--- backd/backd.go
+++ backd/backd.go
@@ -13,13 +13,13 @@ type Backd struct {
 	sling      *sling.Sling
 	authURL    string
 	objectsURL string
-	apiKey     string
+	adminURL   string
 	sessionID  string
 	expiresAt  int64
 }
 
 // NewClient returns an usable client to connect to an instance of Backd
-func NewClient(authURL, objectsURL, apiKey string) *Backd {
+func NewClient(authURL, objectsURL, adminURL string) *Backd {
 
 	var (
 		backd Backd
@@ -27,7 +27,7 @@ func NewClient(authURL, objectsURL, apiKey string) *Backd {
 
 	backd.authURL = authURL
 	backd.objectsURL = objectsURL
-	backd.apiKey = apiKey
+	backd.adminURL = adminURL
 	backd.ConnectionTimeouts(5, 5, 10)
 
 	return &backd
@@ -49,5 +49,30 @@ func (b *Backd) ConnectionTimeouts(dialer, tlsHandshake, timeout time.Duration) 
 			TLSHandshakeTimeout: tlsHandshake * time.Second,
 		},
 	}).Set("User-Agent", clientName)
+
+}
+
+// BootstrapCluster creates the first user on the cluster with full Administration
+// permissions on the the backd application
+func (b *Backd) BootstrapCluster(code, name, username, email, password string) error {
+
+	var (
+		body     BootstrapRequest
+		failure  APIError
+		response *http.Response
+		err      error
+	)
+
+	body = BootstrapRequest{
+		Code:     code,
+		Name:     name,
+		Username: username,
+		Email:    email,
+		Password: password,
+	}
+
+	response, err = b.sling.Post(b.buildPath(admin, pathBootstrap)).BodyJSON(&body).Receive(nil, &failure)
+
+	return failure.wrapErr(err, response, http.StatusCreated)
 
 }
