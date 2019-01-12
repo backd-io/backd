9d758d2ef3e83ec65730ea75c2a20745752d0514
Antonio Fernandez Vara <antoniofernandezvara@gmail.com>
2019-01-12 18:56:52 +0100

    struct for relation

---
+++ internal/structs/relation.go
@@ -0,0 +1,12 @@
+package structs
+
+// Relation is the representation of linked data on the DB.
+type Relation struct {
+	ID            string `json:"_id" bson:"_id"`
+	Source        string `json:"src" bson:"src"`
+	SourceID      string `json:"sid" bson:"sid"`
+	Destination   string `json:"dst" bson:"dst"`
+	DestinationID string `json:"did" bson:"did"`
+	Relation      string `json:"rel" bson:"rel"`
+	Metadata      `json:"_meta" bson:"_meta"`
+}
